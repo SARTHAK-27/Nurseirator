bd4b141bf250074c45631d19fa854ba7c09e4480
aviral singh halsi <aviralreus11@gmail.com>
2020-09-19 03:17:33 +0530

    Initial Commit

---
+++ FLUTTER APP/lib/patient.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:nurseirator/Boxes.dart';
+class patient extends StatefulWidget {
+  @override
+  _patientState createState() => _patientState();
+}
+
+class _patientState extends State<patient> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        color: Colors.lightBlueAccent,
+        child: ListView(
+          children: [
+            box(),
+            box(),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
