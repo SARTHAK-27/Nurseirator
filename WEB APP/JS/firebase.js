a9b6eae9a67e5d822fd13d5f677f810d52f8bdff
Ravineel Singhi <ravineel@pop-os.localdomain>
2020-09-19 13:51:42 +0530

    Signup FIREBASE FUNCTION

--- WEB APP/JS/firebase.js
+++ WEB APP/JS/firebase.js
@@ -59,7 +59,7 @@ function login(){
 }
 
 
-
+//FUNCTION FOR SIGNUP FIREBASE
 function signup(){
 
   document.getElementById('').addEventListener('',function(e){
@@ -67,13 +67,56 @@ function signup(){
     e.preventDefault();
 
     var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
-    var first_name = document.getElementById('');
+    var last_name = document.getElementById('');
+    var pass= document.getElementById('');
+    var mail = document.getElementById('');
+    var ph= document.getElementById('');
+    var adr= document.getElementById('');
+    var cty= document.getElementById('');
+    var ste= document.getElementById('');
+    var zip_code= document.getElementById('');
+    var sex= document.getElementById('');
+    var designation= document.getElementById('');
+    var full_name = first_name.value+' '+last_name.value;
+
+    firebase.auth().createUserWithEmailAndPassword(mail.value,pass.value)
+      .then(function(response){
+
+          uid = firebase.auth().currentUser.uid,
+          firebase.database().ref(desig.value+'\/'+uid).set({
+
+            First_Name  :first_name.value,
+            Last_Name   :last_name.value,
+            Email       :mail.value,
+            Password    :pass.value,
+            role        :desig.value,
+            Phone       :ph.value,
+            Gender      :sex.value,
+            Address     :adr.value,
+            City        :cty.value,
+            State       :ste.value,
+            Zipcode     :zip_code.value
+            })
+            .then(function(response){
+              console.log("Added succesfully")
+              window.location='.html'
+
+          }).catch(function(error){
+            var error_code = error.code;
+            var errorMessage = error.Message;
+            console.log(error_code);
+            console.log(errorMessage);
+    
+            })
+
+
+    }).catch(function(error){
+        var error_code = error.code;
+        var errorMessage = error.Message;
+        console.log(error_code);
+        console.log(errorMessage);
+
+      });
   
   });
 }
